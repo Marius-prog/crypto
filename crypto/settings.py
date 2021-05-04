8a3c92929fb4e5e45eac510305cc4e1a298097bd
Marius Sabaliauskas <rha2r4qef9@privaterelay.appleid.com>
2021-05-04 14:28:27 +0200

    fech news

--- crypto/settings.py
+++ crypto/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crypto_app.apps.CryptoAppConfig',
+
 ]
 
 MIDDLEWARE = [
--- crypto_app/views.py
+++ crypto_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import requests
+import json
 
-# Create your views here.
+
+def home(request):
+    api_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
+    api = json.loads(api_request.content)
+
+    return render(request, 'crypto_app/home.html', {'api': api})
