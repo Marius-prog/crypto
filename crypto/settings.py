120c0f35f9684e16bee3133719dcdacd961b4f9b
Marius Sabaliauskas <rha2r4qef9@privaterelay.appleid.com>
2021-05-04 14:36:36 +0200

    Revert "fech news"
    This reverts commit 8a3c92929fb4e5e45eac510305cc4e1a298097bd.

--- crypto/settings.py
+++ crypto/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crypto_app.apps.CryptoAppConfig',
-
 ]
 
 MIDDLEWARE = [
--- crypto_app/views.py
+++ crypto_app/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
-import requests
-import json
 
-
-def home(request):
-    api_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
-    api = json.loads(api_request.content)
-
-    return render(request, 'crypto_app/home.html', {'api': api})
+# Create your views here.
