dece2c31ab450e8207669d25c8e1aaf4b46bd08f
Marius Sabaliauskas <rha2r4qef9@privaterelay.appleid.com>
2021-05-05 12:36:23 +0200

    last update functions

--- crypto_app/views.py
+++ crypto_app/views.py
@@ -4,7 +4,31 @@ import json
 
 
 def home(request):
+    # crypto prices
+    price_request = requests.get(
+        'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,XLM,ADA,USDT,MIOTA,TRX&tsyms=USD')
+    price = json.loads(price_request.content)
+
+    # crypto news
     api_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
     api = json.loads(api_request.content)
 
-    return render(request, 'crypto_app/home.html', {'api': api})
+    return render(request, 'crypto_app/home.html', {'api': api, 'price': price})
+
+
+def prices(request):
+    if request.method == 'POST':
+
+        quote = request.POST['quote']
+
+        # fix all cases
+        quote = quote.upper()
+
+        crypto_request = requests.get(
+            'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + quote + '&tsyms=USD')
+        crypto = json.loads(crypto_request.content)
+        return render(request, 'crypto_app/prices.html', {'quote': quote, 'crypto': crypto})
+
+    else:
+        notfound = 'Enter the crypto currency symbol into the form above..'
+        return render(request, 'crypto_app/prices.html', {'notfound': notfound})
